a6305acbd8634168110cef7c3ca85effe7fe6b0b
Your Name <adnaneramargho@gmail.com>
2022-04-04 12:57:31 +0600

    review done

--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import useComments from '../../hooks/useComments';
+import HomeComment from '../HomeComment/HomeComment';
 
 const Reviews = () => {
+    const [comments, setComments] = useComments();
     return (
-        <div>
-            <h1>What Our Customers Say</h1>
+        <div >
+            <h1 >What Our Customers Say</h1>
+            <div className='comments'>
+                {
+                    comments.map(comment1 => <HomeComment comment1={comment1}></HomeComment>)
+                }
+            </div>
         </div>
     );
 };
